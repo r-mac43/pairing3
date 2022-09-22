de6bb3c9c4e466dc2403cc7b20819d723bd4abf6
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-09-22 15:45:29 -0700

    Update the project

--- friends.js
+++ friends.js
@@ -5,3 +5,10 @@ for (var i=0; i < numbers.length; i++) {
     console.log(numbers[i]);
   }
 }
+numbers = [5, 22, 32, 74, 77, 79, 83, 99]
+
+for (var i=0; i < numbers.length; i++) {
+  if (numbers[i] > 60) {
+    console.log(numbers[i]);
+  }
+}
